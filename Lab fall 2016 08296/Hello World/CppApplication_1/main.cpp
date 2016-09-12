990f7aacc2d086278b75f2c72fdb650126e0d38a
Nick Stang <Nick Stang>
2016-09-12 15:26:49 -0700

    testing

---
+++ Lab fall 2016 08296/Hello World/CppApplication_1/main.cpp
@@ -0,0 +1,52 @@
+/*
+    To change this license header, choose License Headers in Project Properties.
+    To change this template file, choose Tools | Templates
+    and open the template in the editor.
+    Purpose: first program -> Hello World
+ */
+
+//system libraries
+#include <iostream>     //input/output objects
+using namespace std;    //namespace used in the system library
+
+//user Libraries 
+
+//Global constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    int x;//An integer variable
+    
+    //Input values
+    cout<<"type in an integer variable"<<endl;
+    cin>>x;
+    
+    //Process values -> Map inputs to Outputs
+    x=2*x;
+    
+    //Display Output
+    cout<<"Hello World x = "<<x<<endl;
+    
+    //Exit Program
+    return 0;
+}
+
+/* 
+ * File:   main.cpp
+ * Author: Nick Stang
+ *
+ * Created on August 29, 2016, 9:42 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+
+
