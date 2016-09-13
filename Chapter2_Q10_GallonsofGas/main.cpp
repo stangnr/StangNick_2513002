19daed3779d1327ca92ddfbe6276f0e60a17d74d
stangnr <nstang@student.rcc.edu>
2016-09-12 22:01:27 -0700

    9/12/16

---
+++ Chapter2_Q10_GallonsofGas/main.cpp
@@ -0,0 +1,37 @@
+/* 
+   File:   main
+   Author: Nick Stang
+   Created on September 12, 2016, 9:41 AM
+   Purpose:Homework Chapters 1 and 2  
+ */
+
+//System Libraries
+//Calculating the price of a circuit board that cost $14.95 and sold at a 35%
+// profit.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Variables hold the cost, profit, and price to be sold.
+    double cost = 14.95 , pft, prc;
+    
+    //calculating the amount to add to the cost price.
+    pft = cost * 0.35;
+    
+    //Adding the amount for profit to the initial cost of the circuit board.
+    prc = cost + pft;
+    int round=pft*100+0.5f;
+    prc=round/100.0f;
+    pft=round/100.0f;
+    
+    
+    //Displaying the results for the final price
+    cout << "This program determines how much a circuit board should be sold";
+    cout << " for to make a 35% profit.\n";
+    cout << "Cost to make circuit boards: $"          << cost << endl;
+    cout << "Price to sell the circuit boards for: $" << prc << endl;
+    cout << "Profit per-circuit board: $"             << pft << endl;
+    return 0;
+}
+    
